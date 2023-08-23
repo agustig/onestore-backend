241651ee10537240088926291c3ef2ba2164d258
agustig <agusti.g@icloud.com>
2023-08-23 18:55:48 +0900

    Add authorization, list user, search, & pagination

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,10 +3,18 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    public static $permissions = [
+        'dashboard' => ['super_admin', 'admin'],
+        'user-index' => ['admin'],
+        'payment-update' => ['super_admin']
+    ];
     /**
      * The model to policy mappings for the application.
      *
@@ -21,6 +29,12 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        foreach (self::$permissions as $feature => $roles) {
+            Gate::define($feature, function (User $user) use ($roles) {
+                if (in_array($user->role, $roles)) {
+                    return true;
+                }
+            });
+        }
     }
 }
