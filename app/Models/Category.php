abcc58c08c22d2378631bc15686a1414926ccc1a
agustig <agusti.g@icloud.com>
2023-08-26 01:08:57 +0900

    Add api interface for product and category

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description'];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class)->select([
+            'id',
+            'category_id',
+            'name',
+            'description',
+            'price',
+            'image_url'
+        ]);;
+    }
+}
